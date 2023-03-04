e3c17b6f3980bde008dcea081fe664db6d7d2caf
aageev <x@x.x>
2023-03-04 21:18:31 +0200

    [task-0017] BigData. bronze and silver tables creation DDLs

---
+++ lesson_17/sql/bigdata_tables_create_ddl.sql
@@ -0,0 +1,62 @@
+CREATE TABLE `de-07-ageiev-oleksii-l17.bronze.sales`
+(
+  CustomerId STRING,
+  PurchaseDate STRING,
+  Product STRING,
+  Price STRING,
+  _id STRING(36) NOT NULL,
+  _logical_dt DATE NOT NULL,
+  _job_start_dt TIMESTAMP NOT NULL
+)
+PARTITION BY (_logical_dt, DAY)
+OPTIONS(
+  require_partition_filter = TRUE
+)
+;
+
+CREATE TABLE `de-07-ageiev-oleksii-l17.bronze.customer`
+(
+  Id STRING,
+  FirstName STRING,
+  LastName STRING,
+  Email STRING,
+  RegistrationDate STRING,
+  State STRING,
+  _id STRING(36) NOT NULL,
+  _logical_dt DATE NOT NULL,
+  _job_start_dt TIMESTAMP NOT NULL
+)
+;
+
+
+CREATE TABLE `de-07-ageiev-oleksii-l17.silver.sales`
+(
+  customer_id INT64,
+  purchase_date DATE,
+  product STRING,
+  price STRING,
+  _id STRING(36) NOT NULL,
+  _logical_dt DATE NOT NULL,
+  _job_start_dt TIMESTAMP NOT NULL
+)
+PARTITION BY purchase_date
+OPTIONS(
+  require_partition_filter = TRUE
+)
+;
+
+CREATE TABLE `de-07-ageiev-oleksii-l17.silver.customer`
+(
+  id INT64,
+  first_name STRING,
+  last_name STRING,
+  email STRING,
+  registration_date DATE,
+  state STRING,
+  _id STRING(36) NOT NULL,
+  _logical_dt DATE NOT NULL,
+  _job_start_dt TIMESTAMP NOT NULL
+)
+;
+
+
---
+++ lesson_17/sql/bigdata_tables_delete_ddl.sql
@@ -0,0 +1,5 @@
+DROP TABLE `de-07-ageiev-oleksii-l17.bronze.sales`;
+DROP TABLE `de-07-ageiev-oleksii-l17.bronze.customer`;
+
+DROP TABLE `de-07-ageiev-oleksii-l17.silver.sales`;
+DROP TABLE `de-07-ageiev-oleksii-l17.silver.customer`;
